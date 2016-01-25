c5072149948cae53bf3477d1bd5ee3945411c9c6
unknown <miklebelyakov@gmail.com>
2016-01-25 10:38:37 +0200

    3. Add LocalStorage functional

--- js/main.js
+++ js/main.js
@@ -1,115 +1,169 @@
 (function($) {
-	var methods = {
-		options: {},
-		store: {
-			weather: {
-				country: 'countryName',
-				city: 'cityName',
+	$.fn.weatherApi = function(options) {
+		var settings = {
+			url: 'http://api.openweathermap.org/data/2.5/weather?',
+			general: {
+				appid: 'dcb260f54b95ad139c9a9f91e651b831',
+				lang: 'en',
+			},
+		};
+
+		return this.each(function() {
+			var weather = {
+				country: '',
+				city: '',
 				temp: 0,
 				temp_min: 0,
 				temp_max: 0,
 				weather_img: '',
-			}
-		},
+				weather_back: '',
+			};
 
-		_create: function() {
-			this._getLocation();
-		},
+			var location = {
+				geoData: {
+					lat: '',
+					lon: '',
+				},
+				city: {
+					q: '',
+				}
+			};
 
-		_getLocation: function() {
 			var that = this;
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(this._getData.bind(that), this._selectCity.bind(that));
-			} else {
-				alert('Your browser does not support Geo Location.');
-			}
-		},
+			$.extend(weather, options, JSON.parse(localStorage.getItem('weather' + $(that).index())));
+			_init();
 
-		_selectCity: function() {
-			var that = this;
-			var cache = {};
-			that._renderSelect();
-			$(".city").autocomplete({
-				source: function( request, response ) {
-					var term = request.term;
-					if ( term in cache ) {
-						response(cache[term]);
-						return;
-					}
-					$.ajax({
-						url: "http://gd.geobytes.com/AutoCompleteCity",
-						dataType: "jsonp",
-						data: {
-							q: request.term
-						},
-						success: function( data ) {
-							cache[term] = data;
-							response( data );
+			function _init() {
+				if(weather.city) {
+					_getData(weather.city);
+				} else {
+					_getLocation();
+				}
+			};
+
+			function _getLocation() {
+				if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition(_getData, _selectCity);
+				} else {
+					alert('Your browser does not support Geo Location.');
+				}
+			};
+
+			function _selectCity() {
+				var cache = {};
+				_renderSelect();
+				$(that).find('.city').autocomplete({
+					source: function( request, response ) {
+						var term = request.term;
+						if ( term in cache ) {
+							response(cache[term]);
+							return;
 						}
-					});
-				},
-				minLength: 3,
-				select: function( event, ui ) {
-					var position = ui.item.value;
-					$(".class").css('display', 'none');
-					that._destroyData()._getData(position);
-				},
-			});
-		},
+						$.ajax({
+							url: 'http://gd.geobytes.com/AutoCompleteCity',
+							dataType: 'jsonp',
+							data: {
+								q: request.term
+							},
+							success: function( data ) {
+								cache[term] = data;
+								response( data );
+							}
+						});
+					},
+					minLength: 3,
+					select: function( event, ui ) {
+						var position = ui.item.value;
+						$(that).find('.class').css('display', 'none');
+						_destroyData();
+						_getData(position);
+					},
+				});
+			};
 
-		_getData: function(position){
-			var that = this;
-			var url = 'http://api.openweathermap.org/data/2.5/weather?';
-			var apiKey = 'dcb260f54b95ad139c9a9f91e651b831';
-			var lang = 'en';
-			if(position.coords){
-				var lat = position.coords.latitude;
-				var lon = position.coords.longitude;
-				$.get(url+'lat='+lat+'&lon='+lon+'&appid='+apiKey+'&lang='+lang, this._parseData.bind(that));
-			} else {
-				var city = position;
-				$.get(url+'q='+city+'&appid='+apiKey+'&lang='+lang, this._parseData.bind(that));
-			}
-		},
+			function _getData(position){
+				if(position.coords){
+					location.geoData.lat = position.coords.latitude;
+					location.geoData.lon = position.coords.longitude;
+					$.get(_parseUrl(settings.url, location.geoData, settings.general), _parseData);
+				} else {
+					location.city.q = position;
+					$.get(_parseUrl(settings.url, location.city, settings.general), _parseData);
+				}
+			};
 
-		_parseData: function(data) {
-		  var obj = this.store.weather;
-			var fah = 273.15;
-			obj.temp = Math.round(data.main.temp - fah);
-			obj.temp_min = Math.round(data.main.temp_min - fah);
-			obj.temp_max = Math.round(data.main.temp_max - fah);
-			obj.city = data.name;
-			obj.country = data.sys.country;
-			obj.weather_img = 'http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png';
-			this._renderData();
-		},
+			function _parseUrl(source, location, sett) {
+				return  source + 
+								$.map(location, function(val,key) {return (key+'='+val)}).join('&') +
+								'&' + $.map(sett, function(val,key){return (key+'='+val)}).join('&');
+			};
 
-		_renderSelect: function() {
-			var select = '<input class="city" type="text">';
-			$('.weather-location__name').empty(select);
-			$('.weather__image').after(select);
-		},
+			function _parseData(data) {
+				console.log(data);
+				var obj = weather;
+				var dat = data.main;
+				var fah = 273.15;
+				obj.temp = ((dat.temp>fah)?'+':'') + Math.round(dat.temp - fah);
+				obj.temp_min = ((dat.temp_min>fah)?'+':'') + Math.round(dat.temp_min - fah);
+				obj.temp_max = ((dat.temp_max>fah)?'+':'') + Math.round(dat.temp_max - fah);
+				obj.city = data.name;
+				obj.country = data.sys.country;
+				obj.weather_img = 'http://openweathermap.org/img/w/'+ data.weather[0].icon +'.png';
+				_renderData();
+			};
 
-		_renderData: function() {
-			var that = this;
-			var obj = this.store.weather;
-			var template = '<img class="weather__image" src="' + obj.weather_img + 
-										 '" alt=""><div class="weather-location__name">' + obj.city + ', ' + obj.country +
-										 '</div><div class="weather-temp"><div class="weather-temp__min">' + obj.temp_min + 
-										 '</div><div class="weather-temp__max">' + obj.temp_max + '</div></div>';
-			$('.weather').append(template);
-			$('.weather-location__name').on('click', that._selectCity.bind(this));
-		},
+			function _renderSelect() {
+				var title = '<h3 class="weather-title">Input city</h3>';
+				var select = '<input class="city" type="text">';
+				$(that).find('.weather-location').css('display', 'none');
+				$(that).find('.weather__image').css('display', 'none');
+				$(that).find('.weather-temp').css('display', 'none');
+				$(that).find('.holder').prepend(select);
+				$(that).find('.holder').prepend(title);
+				$(that).find('.city').focus();
+				$(that).find('.city').on('focusout', _getBackSelect);
+			};
 
-		_destroyData: function() {
-			$('.weather').empty();
-			return this;
-		},
-	};
-	$.fn.weatherApi = function() {
-		methods._create();
-  };
-	$('.weather').weatherApi();
+			function _getBackSelect() {
+				var currentCity = $(that).find('.weather-location');
+				if (currentCity.length != 0) {
+					$(that).find('.weather-title').remove();
+					$(that).find('.city').remove();
+					$(currentCity).css('display', 'block');
+					$(that).find('.weather__image').css('display', 'block');
+					$(that).find('.weather-temp').css('display', 'block');
+				};
+			};
+
+			function _renderData() {
+				var obj = weather;
+				var template = '<div class="holder"><p class="weather-location"><input class="save-location"' + 
+											 ' type="checkbox" name="checkbox" id="saveLocation"/>' +
+											 '<span class="weather-location__name">' + obj.city + ', ' + obj.country + 
+											 '</span></p><img class="weather__image" src="' + obj.weather_img + 
+											 '" alt=""><div class="weather-temp"><div class="weather-temp__min">' + 
+											 obj.temp_min + ' <sup>o</sup><span>C ...</span></div>' + 
+											 '<div class="weather-temp__max">... ' + obj.temp_max + 
+											 ' <sup>o</sup><span>C</span></div></div></div>';
+				$(that).append(template);
+				$(that).find('#saveLocation').on('change', saveChooise);
+				$(that).find('.weather-location__name').on('click', _selectCity);
+			};
+
+			function saveChooise() {
+				if($(this).is(':checked')){
+					localStorage.setItem('weather' + $(that).index(), JSON.stringify(weather));
+				} else {
+					localStorage.removeItem('weather' + $(that).index());
+				}
+			};
+
+			function _destroyData() {
+				$(that).empty();
+			};
+		});
+};
+$('.weather').weatherApi();
 })(jQuery);
 
 
